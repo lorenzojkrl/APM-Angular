20a7842b61e63a53494483140bfac7fad64ec4dd
Lorenzo <lorenzozar@gmail.com>
2021-03-02 20:15:50 +0200

    ProductListC as directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   template: `
     <div>
       <h1>{{ pageTitle }}</h1>
-      <div>My first component bla!</div>
+      <pm-products></pm-products>
     </div>
   `,
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule, FontAwesomeModule],
   bootstrap: [AppComponent],
 })
