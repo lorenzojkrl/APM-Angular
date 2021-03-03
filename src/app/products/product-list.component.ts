d2acb09889d6498f1e90cac36896af2e90ee0bdf
Lorenzo <lorenzozar@gmail.com>
2021-03-03 16:45:04 +0200

    Add OnInit lifecylce hook

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from './product';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   // Typescript can actually infer the type when a default value is set
   pageTitle: string = 'Products';
   imageWidth: number = 50;
@@ -49,5 +49,10 @@ export class ProductListComponent {
   // Convention methods after property definition
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  // ngOnInit handles any additional initialization tasks.
+  ngOnInit(): void {
+    console.log('In OnInit');
   }
 }
