f26bbe43c79d7f20f599f90a8e6ea1d468465d9e
Lorenzo <lorenzozar@gmail.com>
2021-03-08 18:20:21 +0200

    setting up http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -15,7 +16,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ConvertToSpacesPipe,
     StarComponent,
   ],
-  imports: [BrowserModule, FormsModule, FontAwesomeModule],
+  imports: [BrowserModule, FormsModule, FontAwesomeModule, HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,67 +1,33 @@
 // Create ProductService class that will become a service once registered with injector
 import { Injectable } from '@angular/core';
 
+// Imports to use instance of http client services
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 import { IProduct } from './product';
 
-// Register class with root injector, 
+// Register class with root injector,
 // Thus service available everywhere
 // then inject in product-list
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  getProducts(): IProduct[] {
-    return [
-      {
-        productId: 1,
-        productName: 'Leaf Rake',
-        productCode: 'GDN-0011',
-        releaseDate: 'March 19, 2019',
-        description: 'Leaf rake with 48-inch wooden handle.',
-        price: 19.95,
-        starRating: 3.2,
-        imageUrl: 'assets/images/leaf_rake.png',
-      },
-      {
-        productId: 2,
-        productName: 'Garden Cart',
-        productCode: 'GDN-0023',
-        releaseDate: 'March 18, 2019',
-        description: '15 gallon capacity rolling garden cart',
-        price: 32.99,
-        starRating: 4.2,
-        imageUrl: 'assets/images/garden_cart.png',
-      },
-      {
-        productId: 5,
-        productName: 'Hammer',
-        productCode: 'TBX-0048',
-        releaseDate: 'May 21, 2019',
-        description: 'Curved claw steel hammer',
-        price: 8.9,
-        starRating: 4.8,
-        imageUrl: 'assets/images/hammer.png',
-      },
-      {
-        productId: 8,
-        productName: 'Saw',
-        productCode: 'TBX-0022',
-        releaseDate: 'May 15, 2019',
-        description: '15-inch steel blade hand saw',
-        price: 11.55,
-        starRating: 3.7,
-        imageUrl: 'assets/images/saw.png',
-      },
-      {
-        productId: 10,
-        productName: 'Video Game Controller',
-        productCode: 'GMG-0042',
-        releaseDate: 'October 15, 2018',
-        description: 'Standard two-button video game controller',
-        price: 35.95,
-        starRating: 4.6,
-        imageUrl: 'assets/images/xbox-controller.png',
-      },
-    ];
+  // define location of json file so Angular CLI can find it
+  // when it serves up the application
+  // define it in angular.json under assets
+  private productUrl = 'api/products/products.json';
+
+  // To use http
+  constructor(private http: HttpClient) {}
+
+  // The method returns an observable of IProduct array
+  getProducts(): Observable<IProduct[]> {
+    // call http get method, pass in the defined url variable
+    // we expect response to be a json structure, containing an array of products
+    // so we set the get method generic parameter to IProduct array
+    // The method will automatically map the response to an array of products
+    return this.http.get<IProduct[]>(this.productUrl);
   }
 }
