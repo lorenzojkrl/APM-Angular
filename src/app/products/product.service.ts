e51547a774c88d002420f1886f6e20d15588c13d
Lorenzo <lorenzozar@gmail.com>
2021-03-09 08:56:14 +0200

    error handling

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,8 +2,10 @@
 import { Injectable } from '@angular/core';
 
 // Imports to use instance of http client services
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+// Error handling
+import { catchError, tap } from 'rxjs/operators';
 
 import { IProduct } from './product';
 
@@ -28,6 +30,29 @@ export class ProductService {
     // we expect response to be a json structure, containing an array of products
     // so we set the get method generic parameter to IProduct array
     // The method will automatically map the response to an array of products
-    return this.http.get<IProduct[]>(this.productUrl);
+    // return this.http.get<IProduct[]>(this.productUrl);
+
+    // Adding exception handling
+    // Add 2 observable operators
+    // tap looks at the values in the stream, without transforming it, ok for logging/debugging
+    // catchError catches errors
+    // Pass operators into pipe method of the Observable
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap((data) => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // Client side or network error occured
+      errorMessage = `An error occured: ${err.error.message}`;
+    } else {
+      // Backend returned unsuccesful response code
+      errorMessage = `Server returned code: ${err.status}, message: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
