3ed03c5db13113a994a85f43992355c1b70cbe78
Lorenzo <lorenzozar@gmail.com>
2021-03-13 19:35:28 +0200

    routerLink and outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,15 @@ import { Component } from '@angular/core';
     <nav class="navbar navbar-expand navbar-light bg-light">
       <a class="navbar-brand">{{ pageTitle }}</a>
       <ul class="nav nav-pills">
-        <li><a class="nav-link">Home</a></li>
-        <li><a class="nav-link">Product List</a></li>
+        <li><a class="nav-link" [routerLink]="['/welcome']">Home</a></li>
+        <li>
+          <a class="nav-link" [routerLink]="['/products']">Product List</a>
+        </li>
       </ul>
     </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
   `,
 })
 export class AppComponent {
